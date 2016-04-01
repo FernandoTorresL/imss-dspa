131033e69161cfb2718dbbbf206745ce929a7667
Fernando Torres-IMSS <fer.torres.laguna@gmail.com>
2016-03-31 20:26:12 -0600

    Primer commit a rama nueva

---
+++ gestionctas/connectvars.php
@@ -0,0 +1,7 @@
+<?php
+  // Define database connection constants
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'dspa_db_user');
+  define('DB_PASSWORD', '');
+  define('DB_NAME', 'dspa_db');
+?>
