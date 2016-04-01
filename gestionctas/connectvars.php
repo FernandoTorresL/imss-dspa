26cdd6fce673c605cefa869e387f1f50308f1746
Fernando Torres-IMSS <fer.torres.laguna@gmail.com>
2016-03-31 19:25:17 -0600

    Creación de archivo .gitignore y preparación para subir rama

--- gestionctas/connectvars.php
+++ gestionctas/connectvars.php
@@ -2,6 +2,6 @@
   // Define database connection constants
   define('DB_HOST', 'localhost');
   define('DB_USER', 'dspa_db_user');
-  define('DB_PASSWORD', '9758-8236.');
+  define('DB_PASSWORD', '75882');
   define('DB_NAME', 'dspa_db');
 ?>
